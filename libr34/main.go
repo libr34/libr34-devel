aefbd07dbe64c0dbcb383ff2a4bccc5d650ca8b9
Susa Milán <mailtosnek@protonmail.com>
2023-01-27 14:26:02 +0100

    Autocomplete

--- libr34/main.go
+++ libr34/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"example.com/libr34/services"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,6 +12,12 @@ func init() {
 
 func main() {
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowAllOrigins:  true,
+		AllowCredentials: false,
+		AllowMethods:     []string{"GET"},
+	}))
 
 	services.AttachServices(r)
 
