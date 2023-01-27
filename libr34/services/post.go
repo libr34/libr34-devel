c81b6d4dd12bc41da3b1028275275a7efcfa0baa
Susa Milán <mailtosnek@protonmail.com>
2023-01-27 12:19:33 +0100

    Added tags query param to the posts

--- libr34/services/post.go
+++ libr34/services/post.go
@@ -14,13 +14,21 @@ const BaseUrl string = "https://api.rule34.xxx/index.php?"
 
 func AttachServices(r *gin.Engine) *gin.Engine {
 	r.GET("/", search)
+	r.GET("/posts", posts)
 
 	return r
 }
 
 func search(c *gin.Context) {
+	c.JSON(200, "test")
+}
+
+func posts(c *gin.Context) {
 	var posts models.Posts
-	response, err := http.Get(BaseUrl + "page=dapi&s=post&q=index&tags=femboy&limit=5")
+
+	tags := c.Query("tags")
+
+	response, err := http.Get(BaseUrl + "page=dapi&s=post&q=index&tags=" + tags + "&limit=5")
 
 	if err != nil {
 		fmt.Println("Error making GET request: ", err)
@@ -42,10 +50,6 @@ func search(c *gin.Context) {
 	fmt.Println(posts.PostS[1].Tags)
 
 	c.JSON(200, "test")
-}
-
-func posts(c *gin.Context) {
-
 }
 
 func post(c *gin.Context) {
