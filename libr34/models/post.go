424a8634e38b671718d1e04685b7df1d6e0638f4
Susa Milán <mailtosnek@protonmail.com>
2023-01-27 15:26:41 +0100

    Added json repr

--- libr34/models/post.go
+++ libr34/models/post.go
@@ -5,33 +5,33 @@ import (
 )
 
 type Posts struct {
-	XMLName xml.Name `xml:"posts"`
-	Count   int      `xml:"count,attr"`
-	Offset  int      `xml:"offset,attr"`
-	PostS   []Post   `xml:"post"`
+	XMLName xml.Name `xml:"posts" json:"name"`
+	Count   int      `xml:"count,attr" json:"count"`
+	Offset  int      `xml:"offset,attr" json:"offset"`
+	PostS   []Post   `xml:"post" json:"posts"`
 }
 type Post struct {
-	Height        int    `xml:"height,attr"`
-	Score         int    `xml:"score,attr"`
-	FileURL       string `xml:"file_url,attr"`
-	ParentID      string `xml:"parent_id,attr"`
-	SampleURL     string `xml:"sample_url,attr"`
-	SampleWidth   int    `xml:"sample_width,attr"`
-	SampleHeight  int    `xml:"sample_height,attr"`
-	PreviewURL    string `xml:"preview_url,attr"`
-	Rating        string `xml:"rating,attr"`
-	Tags          string `xml:"tags,attr"`
-	ID            int    `xml:"id,attr"`
-	Width         int    `xml:"width,attr"`
-	Change        int    `xml:"change,attr"`
-	MD5           string `xml:"md5,attr"`
-	CreatorID     int    `xml:"creator_id,attr"`
-	HasChildren   bool   `xml:"has_children,attr"`
-	CreatedAt     string `xml:"created_at,attr"`
-	Status        string `xml:"status,attr"`
-	Source        string `xml:"source,attr"`
-	HasNotes      bool   `xml:"has_notes,attr"`
-	HasComments   bool   `xml:"has_comments,attr"`
-	PreviewWidth  int    `xml:"preview_width,attr"`
-	PreviewHeight int    `xml:"preview_height,attr"`
+	Height        int    `xml:"height,attr" json:"height"`
+	Score         int    `xml:"score,attr" json:"score"`
+	FileURL       string `xml:"file_url,attr" json:"file_url"`
+	ParentID      string `xml:"parent_id,attr" json:"parent_id"`
+	SampleURL     string `xml:"sample_url,attr" json:"sample_url"`
+	SampleWidth   int    `xml:"sample_width,attr" json:"sample_width"`
+	SampleHeight  int    `xml:"sample_height,attr" json:"sample_height"`
+	PreviewURL    string `xml:"preview_url,attr" json:"preview_url"`
+	Rating        string `xml:"rating,attr" json:"rating"`
+	Tags          string `xml:"tags,attr" json:"tags"`
+	ID            int    `xml:"id,attr" json:"id"`
+	Width         int    `xml:"width,attr" json:"width"`
+	Change        int    `xml:"change,attr" json:"change"`
+	MD5           string `xml:"md5,attr" json:"md5"`
+	CreatorID     int    `xml:"creator_id,attr" json:"creator_id"`
+	HasChildren   bool   `xml:"has_children,attr" json:"has_children"`
+	CreatedAt     string `xml:"created_at,attr" json:"created_at"`
+	Status        string `xml:"status,attr" json:"status"`
+	Source        string `xml:"source,attr" json:"source"`
+	HasNotes      bool   `xml:"has_notes,attr" json:"has_notes"`
+	HasComments   bool   `xml:"has_comments,attr" json:"has_comments"`
+	PreviewWidth  int    `xml:"preview_width,attr" json:"preview_width"`
+	PreviewHeight int    `xml:"preview_height,attr" json:"preview_height"`
 }
