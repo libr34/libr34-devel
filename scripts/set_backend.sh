47135413f047567742791fbfdcb9237200695ad7
Susa Milán <mailtosnek@protonmail.com>
2023-01-30 13:15:28 +0100

    XD

--- scripts/set_backend.sh
+++ scripts/set_backend.sh
@@ -2,7 +2,7 @@
 
 read -p "Where will your backend be hosted? (r34.example.com/api) <- " api_address
 
-files=(search.js searchPosts.js videoPlayer.js viewPost.js)
+files=("search.js" "searchPosts.js" "videoPlayer.js" "viewPost.js")
 
 for file in "${files[@]}"; do
     sed -i "s/APIADDR/${api_address}/g" "scripts/$file"
