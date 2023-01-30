b89ffd4e7ed1c42eeebc2944f2f6541dd8c466d1
Susa Milán <mailtosnek@protonmail.com>
2023-01-30 13:28:18 +0100

    Fix?

--- scripts/set_backend.sh
+++ scripts/set_backend.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 read -p "Where will your backend be hosted? (r34.example.com/api) <- " api_address
 
 files=("search.js" "searchPosts.js" "videoPlayer.js" "viewPost.js")
 
 for file in "${files[@]}"; do
-    sed -i "s/APIADDR/${api_address}/g" "scripts/$file"
+    sed -i s/APIADDR/${api_address}/g scripts/$file
 done
 
 echo "Done!"
