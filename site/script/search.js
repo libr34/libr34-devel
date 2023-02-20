8608e34d3e4073ed1d4e03c6b4ad24af1ea3baf2
Susa Milán <mailtosnek@protonmail.com>
2023-02-20 14:20:35 +0100

    Added basic autocomplete

--- site/script/search.js
+++ site/script/search.js
@@ -4,12 +4,34 @@
     let search = document.getElementById('search')
     search.addEventListener('input', async function() {
         console.log(search.value)
-        const resp = await fetch(`https://APIADDR/api/autocomplete?q=${search.value}`, {
+        if(search.value != undefined && search.value != "") {
+        let autoct = document.getElementById('autoct')
+        autoct.innerText = `\"${search.value}\"`
+        const resp = await fetch(`http://localhost:8080/api/autocomplete?q=${search.value}`, {
+        //const resp = await fetch(`https://APIADDR/api/autocomplete?q=${search.value}`, {
             method: 'GET',
         })
-        console.log(await resp)
         if (await resp.ok) {
-            autoc = await resp.json()
+            autoc = JSON.parse(await resp.json())
             console.log(autoc)
+            let autocomp = document.getElementById('autocomplete')
+            autocomp.style.visibility = 'visible'
+            let autocc = document.getElementById('autocc')
+            autocc.innerHTML = ""
+            for(let e = 0; e < autoc.length; e++) {
+                const newE = document.createElement('a')
+                newE.href = `./posts.html?q=${search.value}&p=0`
+
+                const newContent = document.createTextNode(autoc[e].label)
+                console.log(autoc[e])
+
+                newE.appendChild(newContent)
+
+                autocc.appendChild(newE)
+            }
+        }
+        } else {
+            let autocomp = document.getElementById('autocomplete')
+            autocomp.style.visibility = 'hidden'
         }
     })
